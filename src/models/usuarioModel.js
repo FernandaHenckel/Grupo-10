9a7a7a9605c35b3883ab050620f84a05c7b37c86
rafaeltokko <rafael.tokko@sptech.school>
2025-08-12 21:00:12 -0300

    adição do web-data-viz

---
+++ src/models/usuarioModel.js
@@ -0,0 +1,24 @@
+var database = require("../database/config")
+
+function autenticar(email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucaoSql = `
+        SELECT id, nome, email FROM usuario WHERE email = '${email}' AND senha_hash = sha2('${senha}',256);
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function cadastrar(nome, email, dataNasc, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, dataNasc, senha);
+    var instrucaoSql = `
+        INSERT INTO usuario (nome, email, dataNasc, senha_hash) VALUES ('${nome}', '${email}', '${dataNasc}', sha2('${senha}',256));
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+module.exports = {
+    autenticar,
+    cadastrar
+};

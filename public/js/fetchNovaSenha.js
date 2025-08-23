cab9bcbe93485b9f6a0241f9cf7abbc286e6f3cb
CaioSousavs <joao.sousa@sptech.school>
2025-08-23 19:14:59 -0300

    fazendo fetch de redefinição de senha

---
+++ public/js/fetchNovaSenha.js
@@ -0,0 +1,43 @@
+
+
+function alterar() {
+    var IdUsuario = sessionStorage.ID_USUARIO;
+    var nome = sessionStorage.NOME_USUARIO;
+
+    console.log("id do usuario: " + IdUsuario);
+
+    fetch(`usuario/renovar/${IdUsuario}`,
+        {
+            method: "get",
+            headers: {
+                "Content-Type": "application/json",
+                body: JSON.stringify({
+                    senha: senhaVar,
+                    nova: novaSenhaVar
+                }),
+            },
+        }).then(function (resposta) {
+            console.log("Entrei no fetch");
+            if (resposta.ok) {
+
+                console.log("Senha redefinida com sucesso" + nome);
+
+
+                setTimeout(() => {
+                    window.location = "login.html";
+                }, "2000");
+
+            } else {
+                throw "Houve um erro ao tentar realizar a alteração da senha!"
+
+            }
+
+        })
+        .catch(function (resposta) {
+            console.log(`#ERRO: ${resposta}`);
+        });
+
+        return;
+
+}
+//by:caio 
